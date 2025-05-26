c142323d0284fbb592b0b311559f06e4ab0424fc
subidit123 <subidit556.hitece2020@gmail.com>
2025-05-26 19:41:52 +0530

    Fix

--- frontend-project/vite.config.js
+++ frontend-project/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/Website-Clone"
-})
+  base:process.env.VITE_BASE_PATH  || "/Website-Clone",
+});
